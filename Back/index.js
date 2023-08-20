3c8200a50c4fae22cfa93f9465af7f4e0e51fbac
batman <kumarsinghalok28@gmail.com>
2023-08-20 23:36:13 +0530

    added some libs

--- Back/index.js
+++ Back/index.js
@@ -4,11 +4,14 @@ dotenv.config();
 import cors from 'cors';
 import dbConnection from './config/mongoInit.js';
 dbConnection();
+import flash from 'express-flash';
+import session from 'express-session';
 
 const app = express();
 app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
 
 const PORT = process.env.PORT || 8080;
 
